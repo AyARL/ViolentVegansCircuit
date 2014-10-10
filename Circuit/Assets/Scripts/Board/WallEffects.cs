947ab035d2a6acb6d639bbf2c343ee18639b8450
AyARL <ayamrau@gmail.com>
2014-10-10 18:33:26 +0100

    Added wall particle bursts

---
+++ Circuit/Assets/Scripts/Board/WallEffects.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class WallEffects : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject wallPushEffect = null;
+
+    private ParticleSystem[] particleSystems = null;
+
+    private void Start()
+    {
+        particleSystems = wallPushEffect.GetComponentsInChildren<ParticleSystem>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            wallPushEffect.transform.position = collision.contacts[0].point;
+            foreach (ParticleSystem ps in particleSystems)
+            {
+                //ps.loop = true;
+                ps.Play();
+            }
+        }
+    }
+
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    if (collision.transform.tag == "Player")
+    //    {
+    //        wallPushEffect.transform.position = collision.contacts[0].point;
+
+    //        //if(!particleSystems.Any(p => p.isPlaying))
+    //        //{
+    //        //    foreach (ParticleSystem ps in particleSystems)
+    //        //    {
+    //        //        ps.Play();
+    //        //    }
+    //        //}
+    //    }
+    //}
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            foreach (ParticleSystem ps in particleSystems)
+            {
+                ps.Stop();
+            }
+        }
+    }
+}
