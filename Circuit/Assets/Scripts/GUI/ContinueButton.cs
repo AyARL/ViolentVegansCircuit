e602e0494a5c52ace5121fe9a9874b26d0d823a7
AyARL <ayamrau@gmail.com>
2014-10-13 15:44:21 +0100

    UI assets and Main menu v1

---
+++ Circuit/Assets/Scripts/GUI/ContinueButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ContinueButton : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Button>().interactable = false;
+        GetComponentInParent<MainMenuGUI>().OnContinueAvailable += Enable;
+
+    }
+
+    private void Enable()
+    {
+        GetComponentInParent<MainMenuGUI>().OnContinueAvailable -= Enable;
+        GetComponent<Button>().interactable = true;
+    }
+}
---
+++ Circuit/Assets/Scripts/GUI/MainMenuGUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class MainMenuGUI : MonoBehaviour
+{
+    [SerializeField]
+    private int firstLevelIndex = 0;
+
+    private int lastLevelPlayed = -1;
+
+    public UnityAction OnContinueAvailable { get; set; }
+
+    private void Start()
+    {
+        lastLevelPlayed = PlayerPrefs.GetInt("LastLevelPlayed", -1);
+        if (lastLevelPlayed > 0)
+        {
+            OnContinueAvailable();
+        }
+    }
+
+    public void StartNewGame()
+    {
+        Application.LoadLevel(firstLevelIndex);
+    }
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void SelectLevel()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
